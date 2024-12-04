d1ff2d87cbe88af48482b1f656da8351eb4f5d41
Shubham Mahajan <131528208+Shubham-Mahajan810@users.noreply.github.com>
2024-12-04 16:02:29 +0530

    Update Url.js

--- src/Constants/Url.js
+++ src/Constants/Url.js
@@ -1,1 +1,2 @@
-export const URL_PATH = "http://localhost:2001/Delicious_Dairy"
+// export const URL_PATH = "http://localhost:2001/Delicious_Dairy"
+export const URL_PATH = "https://dd-backend-sky.onrender.com"
